3e8fdb84e48bec25ff7202dd67b5384e1d6f01ef
thomasrbm <frozyxyt.76@gmail.com>
2026-05-01 07:54:11 +0200

    update nixos config

--- configuration.nix
+++ configuration.nix
@@ -1,17 +1,13 @@
 { config, pkgs, ... }:
-
 {
   imports = [ ./hardware-configuration.nix ];
-
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelParams = [ "elevator=none" ];
-
   # Réseau
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-
   # Timezone / Locale
   time.timeZone = "Europe/Paris";
   i18n.defaultLocale = "en_US.UTF-8";
@@ -26,17 +22,14 @@
     LC_TELEPHONE = "fr_FR.UTF-8";
     LC_TIME = "fr_FR.UTF-8";
   };
-
   # Display
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "virtualbox" ];
   services.xserver.xkb = { layout = "us"; variant = ""; };
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
-
   # GNOME extensions activées
   environment.gnome.excludePackages = [];
-
   # Son
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -46,7 +39,6 @@
     alsa.support32Bit = true;
     pulse.enable = true;
   };
-
   # Utilisateur
   users.users.throbert = {
     isNormalUser = true;
@@ -54,38 +46,41 @@
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     shell = pkgs.zsh;
   };
-
   # Programmes
   programs.zsh.enable = true;
   programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
 
+  # nix-ld pour exécuter des binaires dynamiques génériques (Claude Code, etc.)
+  programs.nix-ld = {
+    enable = true;
+    libraries = with pkgs; [
+      stdenv.cc.cc
+      zlib
+      openssl
+      glibc
+    ];
+  };
+
   # Virtualisation
   virtualisation.virtualbox.guest.enable = true;
   virtualisation.docker.enable = true;
-
   # Packages
   environment.systemPackages = with pkgs; [
     # Editeurs
     vscode vim nano gnome-tweaks
-
     # Dev
     git gnumake cmake gcc gdb valgrind clang llvm
-
     gnomeExtensions.arcmenu
-    
-    gnome-terminal 
+    gnome-terminal
     # Docker
     docker docker-compose
-
     # Réseau
     curl wget tcpdump nmap net-tools
-
     # Système
     htop btop tree unzip zip file lsof strace
-
     gnomeExtensions.desktop-icons-ng-ding
-    
+
     (python3.withPackages (ps: with ps; [
       requests
       beautifulsoup4
@@ -95,17 +90,12 @@
       matplotlib
       pillow
       pandas
-      stdenv.cc.cc
-      zlib
-      openssl
     ]))
 
     # Shell
     zsh tmux fzf ripgrep bat eza
-
     # GNOME
     gnomeExtensions.dash-to-dock
   ];
-
   system.stateVersion = "25.11";
 }
